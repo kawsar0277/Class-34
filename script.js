aa3c5dd261460ba77505530abcf35f80ff0ed58c
kawsar0277 <kawsar1252005@gmail.com>
2025-06-27 21:47:10 +0600

    class-34 is aaded in repository

---
+++ script.js
@@ -0,0 +1,22 @@
+let rahim;
+let karim;
+let rafiq;
+
+
+rahim = "cycle";
+karim = "bike";
+rafiq = "car";
+
+
+if(rahim == "cycle")
+    console.log('I will go to market')
+
+else if(karim == 'bike')
+    console.log('I will go to playground')
+
+else if(rafiq == 'car')
+    console.log('I will go to park')
+
+else{
+    console.log('I will go to sleep')
+}
